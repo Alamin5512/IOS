dccbe34627fcb4eb0e41dd87b052b2ac48f3300c
Alamin5512 <56962724+Alamin5512@users.noreply.github.com>
2021-11-06 17:34:48 +0600

    Add files via upload

---
+++ fairuznew2/viewcontrollers/UploadViewController.swift
@@ -0,0 +1,32 @@
+//
+//  UploadViewController.swift
+//  fairuznew2
+//
+//  Created by kuetcse on 4/11/21.
+//  Copyright © 2021 kuet. All rights reserved.
+//
+
+import UIKit
+
+class UploadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
